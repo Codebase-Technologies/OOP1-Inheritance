da3631fba3e3bc310fb3502d3309b423e2347605
David Adele <david.adele@hotmail.com>
2022-09-05 14:22:34 +0100

    Add project files.

---
+++ OOP1/AnotherClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP1;
+
+namespace OOP2
+{
+    internal class AnotherClass
+    {
+        void AnotherMethod()
+        {
+            GeneratorPro PowerDubi = new GeneratorPro();
+
+        }
+    }
+
+    class School
+    {
+        public int NoOfClassroom { get; set; }
+        public string SchoolName { get; set; }
+
+        void Teaching(string teacher, string className)
+        {
+
+            Console.WriteLine("Teaching is in progress");
+        }
+
+        public int NoOfTerms { get; set; }
+
+        public int Holidays { get; set; }
+    }
+
+    class HighSchool : School
+    {
+        int Flogging(int caneStrength, string studentName)
+        {
+
+            return 7;
+        }
+    }
+
+    class College : School
+    {
+
+    }
+
+    class JAMB
+    {
+        void Apply()
+        {
+            HighSchool mySchool = new HighSchool();
+            mySchool.SchoolName = "FUOYE";
+        } 
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int theGreaterNumEver = Int32.MaxValue;
+            GeneratorPro PelumGenPro = new GeneratorPro();
+            PelumGenPro.GenLabel = "PelumiPower!";
+        }
+
+    }
+
+    internal class GeneratorPro
+    {
+        private string GenLabel { get; set; }
+
+        private int serialNoId;
+
+        void Ignition(int petrolQuan)
+        {
+            Console.WriteLine($"{GenLabel} has started working");
+        }
+    }
+}
