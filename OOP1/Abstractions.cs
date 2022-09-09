8df39e718b43fc7be536a6f92a571a88a1b0dfdc
David Adele <david.adele@hotmail.com>
2022-09-09 13:47:58 +0100

    abstraction example

---
+++ OOP1/Abstractions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal abstract class Animal
+    {
+        public int Legs { get; set; }
+
+    }
+
+    class Normal
+    {
+        void DoNormal(){
+            Animal pet = new Animal();
+        }
+    }
+
+
+   public abstract class VehicleUsage
+    {
+        public int Tyres { get; set; }
+
+        void ChangeTyres(int tyres)
+        {
+            Console.WriteLine("Change the faulty tyres");
+        }
+
+        public abstract void RepairExhaust();
+        
+        double Driving(decimal petrolQuantity)
+        {
+            return 50;
+        }
+    }
+
+   class MechanicUsage : VehicleUsage
+   {
+       public override void RepairExhaust()
+       {
+           Console.WriteLine("I will check and fix from the manifold");
+       }
+
+    }
+}
